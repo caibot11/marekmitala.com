a80f99b3d0880313c1706102ad229c9cda0eb1e8
Marek Mitala <i559160@fontysict.nl>
2025-02-17 11:30:41 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,21 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 
-export const metadata: Metadata = {
-  title: "Marek Mitala"
+export const metadata = {
+  title: 'Marek Mitala',
+  description: 'Marek Mitala`s personal website',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      {url: '/android-chrome-192x192.png', siezes: '192x192',type:'image/png'},
+      {url: '/android-chrome-512x512.png', siezes: '512x512',type:'image/png'},
+
+
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
